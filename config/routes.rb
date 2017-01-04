15ec492698a272a5eefefd3e8bd5239655fbadf2
你的名字 <pincha120@gmail.com>
2017-01-04 22:50:10 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,13 @@ Rails.application.routes.draw do
 
     resources :posts
   end
-  root 'groups#index'
+
+  namespace :account do
+    resources :groups
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  root 'groups#index'
 end
